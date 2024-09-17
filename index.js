e68254252c2b803995f2f04360ace64641a36d75
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-09-17 12:02:00 +0100

    logged in to VerboChat through express server

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
-require("dotenv").config();
-// const apiKey = process.env.API_KEY;
-// const apiSecret = process.env.API_SECRET;
-// console.log("API Key:", apiKey);
-// console.log("API Secret:", apiSecret);
+import dotenv from "dotenv"; // Import dotenv using ES6 syntax
+dotenv.config(); // Configure dotenv to load .env file
+import commands from "./commands/commands.js";
+import events from "./events/events.js";
+import testing from "./testing/tests.js";
+import express from "express"; // Import express
 
-console.log("server running");
+import * as Discord from "discord.js"; // Imports discord.js
+
+const client = new Discord.Client({
+  intents: [
+    Discord.GatewayIntentBits.Guilds,
+    Discord.GatewayIntentBits.GuildMessages,
+  ],
+}); //creates new client
+
+const discordToken = process.env.DISCORD_TOKEN;
+
+const app = express(); // Initialize express
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+//this line must be at the very end
+client.login(discordToken); //signs the bot in with token
+
+// Define a simple get route to display 'Hello World' on the route of the server.
+// app.get("/", (req, res) => {
+//     res.send("Hello, World!");
+
+// });
+
+// app.push("/", (req, res) => {
+//   res.send("hello World");
+// });
+
+// app.delete("/");
+
+// This is listening to port 3000 and showing in the node terminal.
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
