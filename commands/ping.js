07044c5421941c30a677f8d35f862880dcab7a71
MarikaBBB <mbertelli012@gmail.com>
2024-09-17 12:04:54 +0100

    ping command

---
+++ commands/ping.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Responds with Pong! 🏓"),
+  async execute(interaction) {
+    await interaction.reply("Pong! 🏓");
+  },
+};
--- index.js
+++ index.js
@@ -3,3 +3,28 @@ const apiKey = process.env.API_KEY;
 const apiSecret = process.env.API_SECRET;
 console.log("API Key:", apiKey);
 console.log("API Secret:", apiSecret);
+
+
+// Loading command files
+// const fs = require('node:fs');
+// const path = require('node:path');
+// const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+// const { token } = require('./config.json');
+
+// const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// client.commands = new Collection();
+
+
+// command handler
+// client.commands = new Collection();
+
+// const commandFiles = fs
+//   .readdirSync("./commands")
+//   .filter((file) => file.endsWith(".js"));
+
+// for (const file of commandFiles) {
+//   const command = require(`./commands/${file}`);
+//   client.commands.set(command.data.name, command);
+//   commands.push(command.data.toJSON());
+// }
