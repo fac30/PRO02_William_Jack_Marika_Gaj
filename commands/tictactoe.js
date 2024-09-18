9f28bed5f8863fc7b3d58edf4262cc5c8011b054
william-man <william.man@uymail.com>
2024-09-18 16:30:44 +0100

    tic tac toe update

--- commands/tictactoe.js
+++ commands/tictactoe.js
@@ -37,5 +37,40 @@ export default {
       content: "Tic Tac Toe started! Click on a button to play!",
       components: createGrid(),
     });
+
+    const filter = (i) => {
+      i.customId.startsWith("ttt_") && i.user.id === interaction.user.id;
+    };
+    const collector = interaction.channel.createMessageComponentCollector({
+      filter,
+      componentType: ComponentType.Button,
+      time: 60000, // 1 minute timeout
+    });
+
+    collector.on("collect", async (i) => {
+      const [_, row, col] = i.customId.split("_");
+      const rowIndex = parseInt(row);
+      const colIndex = parseInt(col);
+
+      // Update the board
+      if (board[rowIndex][colIndex] === "⬜") {
+        board[rowIndex][colIndex] = currentPlayer;
+        currentPlayer = currentPlayer === "❌" ? "⭕" : "❌";
+      }
+
+      // Check for win condition or draw (this can be added later)
+
+      await i.update({
+        content: `Tic Tac Toe ongoing! ${currentPlayer}'s turn`,
+        components: createGrid(),
+      });
+    });
+
+    collector.on("end", async () => {
+      await interaction.editReply({
+        content: "Tic Tac Toe game ended.",
+        components: [],
+      });
+    });
   },
 };
